1dac0e312dc7e5c54eb36ef7217de3684d487a73
RafaelDellaValle <rafaeldesousa3245@gmail.com>
2025-08-26 00:15:06 -0300

    primeiro commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <div>
+         <h1>Localização</h1>
+      </div>
+
+      <div>
+        <img src="./" alt="Imagem " />
+        <h1>20</h1>
+      </div>
+       
+       <div>
+          <ul>
+            <li>seg.</li>
+            <li>ter.</li>
+            <li>qua.</li>
+            <li>qui.</li>
+            <li>sex.</li>
+            <li>sáb.</li>
+            <li>dom.</li>
+          </ul>
+       </div>
+
+
+
+
+
+    </div>
+    
+  )
+}
+
+export default App
