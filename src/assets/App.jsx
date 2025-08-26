5a81a43fdceed71e3e0aa29636f0f4231461a507
RafaelDellaValle <rafaeldesousa3245@gmail.com>
2025-08-26 01:34:14 -0300

    adicionando lógica e api

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,19 +1,54 @@
+import { useState, useEffect, useRef } from "react";
+import axios from "axios"
+
+import sol from "../../public/images/sol.png";
+import chuva from "../../public/images/chuva.png";
+import nuvem from "../../public/images/nebuloso.png";
 import "./App.css";
 
 function App() {
+  const [clima, setClima] = useState(null);
+  const [temp, setTemp] = useState(null);
+
+   const imagens = {
+    Clear: sol,
+    Rain: chuva,
+    Clouds: nuvem,
+  };
+
+  const apiKey = "e84ed6d73294a4a874405b59f0a21926";
+  const idCidade = "3448439" //São Paulo
+
+  const inputRef = useRef()
+
+   async function searchCity() {
+      console.log(inputRef.current.value)
+      const city = inputRef.current.value
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
+
+      const data = await axios.get(url)
+
+      console.log(data)
+    }
+
 
   return (
-    <div className="caixa">
-      <div>
-         <h1>Localização</h1>
-      </div>
+    <div >
 
-      <div>
-        <img src="../../public/images/chuva.png" alt="Imagem " className="iconeClima" />
-        <h1>20</h1>
+
+
+
+      <div className="caixa">
+          <input ref={inputRef} type="text" placeholder="Digite o nome da sua cidade" />
+          <button onClick={searchCity}></button>
+
+         <h1>Localização</h1>
+        <img src={imagens[clima]} alt={clima} className="iconeClima" />
+        <h1>{temp}</h1>
       </div>
        
-       <div>
+       <div className="caixa2">
           <ul>
             <li>seg.</li>
             <li>ter.</li>
